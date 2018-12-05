17631f2da4f683ba4bd228ae6f397960c87c27a5
Odd Karsten <oksvart@gmail.com>
2018-12-05 14:18:51 +0100

    Initial commit, basic Go json response implemented

---
+++ OddCalculate.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"math"
+)
+
+type CalcEvent struct {
+	Number1  int    `json:"Number 1"`
+	Number2  int    `json:"Number 2"`
+	Operator string `json:"Operator"`
+}
+
+type CalcResponse struct {
+	Result string `json:"Result:"`
+}
+
+func HandleLambdaEvent(event CalcEvent) (CalcResponse, error) {
+	return CalcResponse{
+		//result from calculation
+		Result: "test" 
+	},
+	nil
+}
