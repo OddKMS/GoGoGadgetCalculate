34142c7a74a006df583137218c02feb5bb2240af
Odd Karsten <oksvart@gmail.com>
2018-12-06 13:58:36 +0100

    Build script completed.

--- build.ps1
+++ build.ps1
@@ -1,1 +1,10 @@
 #Build-script for GoCalculate
+$env:GOOS="linux"
+$goPath = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent;
+$goZipCmd = $goPath + "\bin\build-lambda-zip.exe -o GoCalculate.zip GoCalculate";
+
+go build -o GoCalculate;
+
+Invoke-Expression $goZipCmd;
+#reset the GOOS env-var to let me build to .exe in VSCode directly from terminal
+$env:GOOS="windows"
